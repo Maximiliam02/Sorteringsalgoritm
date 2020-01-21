bbb0e1390c1b988416ec42180fb6447ae78fe814
Maximiliam02 <54435275+Maximiliam02@users.noreply.github.com>
2020-01-21 13:18:49 +0100

    koden
    kod

---
+++ program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace sfdfwedfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] A = new int[10000];
+
+
+            int temp;
+            Random Talet = new Random();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = Talet.Next(A.Length);
+
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int b = 0; b < A.Length - 1 - i; b++)
+                {
+
+                    if (A[b] > A[b + 1])
+                    {
+                        temp = A[b];
+                        A[b] = A[b + 1];
+                        A[b + 1] = temp;
+
+
+
+                    }
+
+
+                }
+
+            }
+            stopwatch.Stop();
+            for (int x = 0; x < A.Length; x++)
+            {
+                Console.WriteLine(A[x]);
+            }
+            Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds);
+        }
+    }
+}
+            
+
+
